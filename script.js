acce3fe5eae35ffaf1397ddfe9796f8f160c7d5f
mihaitruta <mihai.truta1996@gmail.com>
2024-05-16 12:28:04 +0300

    breathing circle #1

--- script.js
+++ script.js
@@ -1,117 +1,72 @@
 let size = 400;
-let squares = [];
-let squareSize = size / 20;
-let pathSize = size / 2;
-let path = [];
-let start = size / 2 - pathSize / 2;
-const positionsPerStep = 30;
-const squarePlacements = [0, 0.22, 0.41, 0.59, 0.78, 1];
+let maxSize = 200;
+let minSize = 50;
+let currentSize = minSize;
+let growing = true;
+let speed = 1;
+let iter = 0;
+let colorStart;
+let colorEnd;
+let middleSize;
+let distanceFromMiddle;
+let distanceFromMiddleNormalized;
 
-function createPositions() {
-  addPositionsForSide("top", start, start);
-  addPositionsForSide("right", start + pathSize, start);
-  addPositionsForSide("bottom", start, start + pathSize);
-  addPositionsForSide("left", start, start);
-}
-
-function addPositionsForSide(side, x, y) {
-  for (let p = 0; p < squarePlacements.length - 1; p++) {
-    const offset =
-      side == "top" || side == "right"
-        ? pathSize * squarePlacements[p]
-        : pathSize - pathSize * squarePlacements[p];
-    const start = side == "top" || side == "bottom" ? x : y;
-    const positionStart = start + offset;
-    const step =
-      (pathSize * squarePlacements[p + 1] - pathSize * squarePlacements[p]) /
-      positionsPerStep;
-    for (let i = 0; i < positionsPerStep; i++) {
-      const adjustment =
-        side == "top" || side == "right" ? step * i : -step * i;
-      path.push({
-        x: side === "top" || side === "bottom" ? positionStart + adjustment : x,
-        y: side === "left" || side === "right" ? positionStart + adjustment : y
-      });
-    }
-  }
-}
-
-class Square {
-  constructor(position, size, color) {
-    this.position = position;
-    this.size = size;
-    this.color = color;
-  }
-
-  move() {
-    this.position++;
-    if (this.position >= path.length) {
-      this.position = 0;
-    }
-  }
-
-  draw() {
-    stroke(this.color);
-    strokeWeight(this.size * 0.14);
-    square(
-      path[this.position].x - this.size / 2,
-      path[this.position].y - this.size / 2,
-      this.size
-    );
-  }
-}
-
-function createSquares() {
-  for (let i = 0; i < path.length; i += positionsPerStep) {
-    squares.push(
-      new Square(
-        i,
-        squareSize,
-        color(Math.floor((i / path.length) * 360), 100, 34)
-      )
-    );
-  }
-}
-
-// p5 methods
 function setup() {
   const c = createCanvas(size, size);
   c.parent("canvas");
   noFill();
   colorMode(HSL);
-  resize();
+  colorStart = color(200, 100, 50); // Starting color for gradient
+  colorEnd = color(300, 100, 50); // Ending color for gradient
 }
 
 function draw() {
+  iter = (iter + 1) % 1000;
   clear();
-  strokeWeight(1);
-  // square(start, start, pathSize);
-  for (let i = 0; i < squares.length; i++) {
-    const square = squares[i];
-    square.draw();
-    square.move();
+
+  let steps = 50;
+  for (let i = steps; i > 0; i--) {
+    let inter = map(i, 0, steps, 0, 1);
+    inter = inter * distanceFromMiddleNormalized;
+    let c = lerpColor(colorStart, colorEnd, inter);
+    fill(c);
+    noStroke();
+    ellipse(width / 2, height / 2, currentSize * (i / steps), currentSize * (i / steps));
+  }
+
+  middleSize = (maxSize + minSize) / 2;
+  distanceFromMiddle = abs(currentSize - middleSize);
+  distanceFromMiddleNormalized = distanceFromMiddle / (middleSize - minSize);
+  if (iter % 117 === 1) {
+    console.log('middleSize', middleSize)
+    console.log('abs', abs(currentSize - middleSize));
+    console.log('distanceFromMiddleNormalized', distanceFromMiddleNormalized);
+  }
+  let speed = (1.05 - distanceFromMiddleNormalized) * 3;
+  if (growing) {
+    currentSize += speed;
+    if (currentSize >= maxSize) {
+      growing = false;
+      setTimeout(() => {}, 1000)
+    }
+  } else {
+    currentSize -= speed;
+    if (currentSize <= minSize) {
+      growing = true;
+      setTimeout(() => {}, 1000)
+    }
   }
 }
 
-function resize() {
+
+function windowResized() {
   if (windowWidth < 420) {
     size = windowWidth * 0.8;
   } else {
     size = 400;
   }
 
-  pathSize = size / 2;
-  squareSize = size / 20;
-  start = size / 2 - pathSize / 2;
   resizeCanvas(size, size);
-  squares = [];
-  path = [];
-  createPositions();
-  createSquares();
-}
-
-function windowResized() {
-  resize();
 }
 
 requestAnimationFrame(() => {});
